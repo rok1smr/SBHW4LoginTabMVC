ca2486001486ae3368ed85be43c119d86a114def
ROK1SMR <runnerbkln@gmail.com>
2021-10-30 09:19:52 +0300

    Initial Commit

---
+++ SBHW4LoginTabMVC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SBHW4LoginTabMVC
+//
+//  Created by Konstantin on 30.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
